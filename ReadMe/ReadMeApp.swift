fecfa7696d88634e3b79702cbfdc5efee7b4974a
Oscar Cristaldo <fabianskier@icloud.com>
2021-01-02 10:26:41 -0300

    Initial Commit

---
+++ ReadMe/ReadMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReadMeApp.swift
+//  ReadMe
+//
+//  Created by Oscar Cristaldo on 2021-01-02.
+//
+
+import SwiftUI
+
+@main
+struct ReadMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
